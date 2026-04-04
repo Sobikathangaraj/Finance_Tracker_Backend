25adbe03fd1bc7b1f85c875c8a5a4a5bf39a7e52
Sobikathangaraj <141642789+Sobikathangaraj@users.noreply.github.com>
2026-04-04 13:01:07 +0530

    Add 'viewer' role to dashboard routes

--- routes/dashboardrouter.js
+++ routes/dashboardrouter.js
@@ -10,9 +10,9 @@ var verifyToken = require("../middleware/auth");
 var roleCheck = require("../middleware/rolecheck");
 
 // admin + analyst only
-router.get("/summary", verifyToken, roleCheck("admin", "analyst"), getSummary);
-router.get("/category-totals", verifyToken, roleCheck("admin", "analyst"), getCategoryTotals);
-router.get("/monthly-trends", verifyToken, roleCheck("admin", "analyst"), getMonthlyTrends);
-router.get("/recent-activity", verifyToken, roleCheck("admin", "analyst"), getRecentActivity);
+router.get("/summary", verifyToken, roleCheck("admin", "analyst","viewer"), getSummary);
+router.get("/category-totals", verifyToken, roleCheck("admin", "analyst", "viewer"), getCategoryTotals);
+router.get("/monthly-trends", verifyToken, roleCheck("admin", "analyst", "viewer"), getMonthlyTrends);
+router.get("/recent-activity", verifyToken, roleCheck("admin", "analyst", "viewer"), getRecentActivity);
 
 module.exports = router;
