1b931bc17655ab08eaa770f8a8f46b5aedca5287
Sobikathangaraj <141642789+Sobikathangaraj@users.noreply.github.com>
2026-04-04 13:22:52 +0530

    Change dashboard routes to allow all roles access
    Updated role permissions to allow all roles to access the dashboard routes.

--- routes/dashboardrouter.js
+++ routes/dashboardrouter.js
@@ -9,10 +9,12 @@ var {
 var verifyToken = require("../middleware/auth");
 var roleCheck = require("../middleware/rolecheck");
 
-// admin + analyst only
+//  all roles can view dashboard
 router.get("/summary", verifyToken, roleCheck("admin", "analyst","viewer"), getSummary);
 router.get("/category-totals", verifyToken, roleCheck("admin", "analyst", "viewer"), getCategoryTotals);
 router.get("/monthly-trends", verifyToken, roleCheck("admin", "analyst", "viewer"), getMonthlyTrends);
 router.get("/recent-activity", verifyToken, roleCheck("admin", "analyst", "viewer"), getRecentActivity);
 
 module.exports = router;
+
+
